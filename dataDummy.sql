e61bab21dc2bc4bd962abe665852aec9a9d48698
florenthiakezia <florent.kezia@gmail.com>
2021-06-10 17:57:17 +0700

    data dummy

---
+++ dataDummy.sql
@@ -0,0 +1,53 @@
+INSERT INTO Member(nama,email,kontak,terverifikasi,alamat,password,statusLangganan)
+VALUES('Joni','Joni@gmail.com','08123435862',1,'Jalan Mawar no 3','joni134',1)
+INSERT INTO Member(nama,email,kontak,terverifikasi,alamat,password,statusLangganan) 
+VALUES('Ana','Ana@gmail.com','08127425563',1,'Jalan Kencana no 1','ana123',0)
+INSERT INTO Member(nama,email,kontak,terverifikasi,alamat,password,statusLangganan) 
+VALUES('Budi','Budi@gmail.com','08736928172',0,'Jalan Kopo no 198','Budidi23',1)
+INSERT INTO Member(nama,email,kontak,terverifikasi,alamat,password,statusLangganan) 
+VALUES('Marie','Marie@gmail.com','08793458291',0,'Jalan Merdeka no 30','Marie1',0)
+INSERT INTO Member(nama,email,kontak,terverifikasi,alamat,password,statusLangganan) 
+VALUES('Evelyne','Evelyne@gmail.com','08729182931',1,'Jalan Cempaka no 88','Eve87',0)
+
+INSERT INTO Admin(email,password,nama)
+VALUES('Bob@gmail.com','bobi23','Bobby')
+INSERT INTO Admin(email,password,nama)
+VALUES('Lukas@gmail.com','Luk98','Lukas')
+INSERT INTO Admin(email,password,nama)
+VALUES('Chelsea@gmail.com','Chel976','Chelsea')
+INSERT INTO Admin(email,password,nama)
+VALUES('Kath23@gmail.com','Kath99','Kathleen')
+INSERT INTO Admin(email,password,nama)
+VALUES('Evan@gmail.com','Evan77','Evander')
+
+INSERT INTO Artikel
+VALUES(1,1,1,'Rahasia Mengatur Keuangan','2019-09-12 21:19:01',null,1)
+INSERT INTO Artikel 
+VALUES(2,0,0,'Rahasia Hidup Sehat','2019-10-03 10:18:29',null,2)
+INSERT INTO Artikel 
+VALUES(3,1,3,'Rahasia Hidup Sukses',null,null,3)
+INSERT INTO Artikel 
+VALUES(4,0,2,'Peluang Usaha Online Tanpa Modal','2019-11-05 23:21:40','2020-02-17 20:09:08',4)
+INSERT INTO Artikel 
+VALUES(5,1,3,'Makanan Hemat Untuk Anak Kosan',null,null,5)
+
+INSERT INTO Membaca 
+VALUES(1,2,'mulai','2020-01-02 23:00:00')
+INSERT INTO Membaca 
+VALUES(1,2,'selesai','2020-01-04 00:00:00')
+INSERT INTO Membaca 
+VALUES(2,1,'mulai','2020-02-29 19:00:00')
+INSERT INTO Membaca 
+VALUES(2,1,'selesai','2020-03-01 18:00:00')
+INSERT INTO Membaca 
+VALUES(3,3,'mulai','2020-04-01 08:00:09')
+INSERT INTO Membaca 
+VALUES(3,3,'selesai','2020-04-01 19:00:00')
+INSERT INTO Membaca 
+VALUES(4,5,'mulai','2020-04-19 21:00:10')
+INSERT INTO Membaca 
+VALUES(4,5,'selesai','2020-04-19 23:00:10')
+INSERT INTO Membaca 
+VALUES(5,4,'mulai','2020-05-29 23:00:20')
+INSERT INTO Membaca 
+VALUES(5,4,'selesai','2020-04-30 23:00:11')
